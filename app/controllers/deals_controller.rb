2fdb85f8c971bccc2a97c2a1233fe866b0d3fac9
albertbahia <albert.bahia1@gmail.com>
2014-09-01 02:08:24 -0400

    added index action back to deals controller, removed deals_search action

--- app/controllers/deals_controller.rb
+++ app/controllers/deals_controller.rb
@@ -1,28 +1,14 @@
 class DealsController < ApplicationController
 	before_filter :authenticate_user!
-	
-	def show
-		@deal = Deal.find(params[:id])
-		render status: 200, json: @deal.to_json
-	end
 
-	def deals_search
-		category = 'outdoor-adventures'
-		@sqoot = Sqoot.search(category)
-		@sqoot.each do |deal|
-			Deal.create({
-				title: deal['deal']['title'],	
-				category: deal['deal']['category_slug'],
-				description: deal['deal']['description'],
-				image_url: deal['deal']['image_url'],
-				expiration_date: deal['deal']['expires_at'],
-				merchant_id: deal['deal']['merchant']['id']
-			})
-		end
+	def index
 		@deals = Deal.all
 		render status: 200, json: @deals.to_json
 	end
 
-
+	def show
+		@deal = Deal.find(params[:id])
+		render status: 200, json: @deal.to_json
+	end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,5 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, :matches
   resources :interests, only: [:index]
-  resources :deals, only:[:show]
-  get '/deals' => 'deals#deals_search' 
+  resources :deals, only:[:index, :show] 
 end
