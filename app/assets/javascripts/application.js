3898096f806b80b8b63cbd60ad95ace58e1bb696
albertbahia <albert.bahia1@gmail.com>
2014-09-01 22:09:26 -0400

    removed ember environment configs, generated backbone file structure.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,14 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require underscore
+//= require backbone
+//= require handlebars
+//= require_tree ./backbone/routers
+//= require_tree ./backbone/models
+//= require_tree ./backbone/collections
+//= require_tree ./backbone/views
+//= require_tree ./templates
 //= require bootstrap-sprockets
 //= require_tree .
 
