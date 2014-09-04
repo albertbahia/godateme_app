50f43a9a115122935dbe464ff8a11c88aceb64e1
albertbahia <albert.bahia1@gmail.com>
2014-09-03 21:40:26 -0400

    modified seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,8 @@
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
-user = CreateAdminService.new.call
-puts 'CREATED ADMIN USER: ' << user.email
+# user = CreateAdminService.new.call
+# puts 'CREATED ADMIN USER: ' << user.email
 
 # Seed data for interests
 url = URI.escape('http://api.sqoot.com/v2/categories?api_key=e158uj')
