a5f03d081ac6564c873ed3268f63ec319a2a66c6
albertbahia <albert.bahia1@gmail.com>
2014-09-03 21:19:16 -0400

    cleaned up some tests and some styling mods

--- spec/models/match_spec.rb
+++ spec/models/match_spec.rb
@@ -3,7 +3,6 @@ describe Match do
   let(:match1) { Match.new({
     user_id1: 1,
     user_id2: 2,
-    date_deal_id: 5
   })  }
 
   it 'is valid with two user id\'s and a date_deal_id' do
@@ -13,17 +12,8 @@ describe Match do
   it 'is invalid without a user id' do
     match2 = Match.new({
       user_id1: 2,
-      date_deal_id: 3
     })
     expect(match2).to have(1).errors_on(:user_id2)
-  end
-
-  it 'is invalid without a date_deal_id' do
-    match3 = Match.new({
-      user_id1: 1,
-      user_id2: 3
-    })
-    expect(match3).to have(1).errors_on(:date_deal_id)
   end
 
   it 'is invalid without two user id\'s' do
