3c11f6affde356a27691f285ae6fe037c6fd32f0
albertbahia <albert.bahia1@gmail.com>
2014-09-15 15:57:28 -0400

    added location columns to User model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140903182139) do
+ActiveRecord::Schema.define(version: 20140915195439) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,6 +55,9 @@ ActiveRecord::Schema.define(version: 20140903182139) do
     t.integer  "age"
     t.integer  "interest_category_id"
     t.string   "interest_category"
+    t.string   "city"
+    t.string   "state"
+    t.string   "country"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
