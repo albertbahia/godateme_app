68a4cbf9964f89fa6094bf25692709a4096e7417
albertbahia <albert.bahia1@gmail.com>
2014-09-16 15:47:52 -0400

    created api key secret variable

--- app/models/sqoot.rb
+++ app/models/sqoot.rb
@@ -1,7 +1,7 @@
 class Sqoot
 
 	def self.search(category)
-		url = URI.escape("http://api.sqoot.com/v2/deals?api_key=e158uj&category_slugs=#{category}")
+		url = URI.escape("http://api.sqoot.com/v2/deals?api_key=#{ENV["SQOOT_API_KEY"]}&category_slugs=#{category}")
 		request = HTTParty.get(url)
 		deals_aray = request['deals']
 	end
