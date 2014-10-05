57d6db82981d76d19df50ebc2ee75a8ba4a93f08
albertbahia <albert.bahia1@gmail.com>
2014-10-05 15:38:24 -0400

    added new route, modified root view

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   root to: 'application#index'
+  get '/main', to: 'application#main'
   devise_for :users, :controllers => { registrations: 'registrations' }
   resources :users, only: [:index, :show]
   resources :matches
   resources :interests, only: [:index]
-  resources :deals, only:[:index, :show] 
+  resources :deals, only:[:index, :show]
 end
